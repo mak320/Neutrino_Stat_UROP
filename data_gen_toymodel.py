7c3c36491e68c34ac34899fa55bc493706f9ef6c
Mate Koszta <matekoszta@gmail.com>
2023-07-06 17:49:48 +0100

    added some basic visualisation, can see plot on overleaf: SOMETHING IS WRONG WITH THE KINEMATICS

--- data_gen_toymodel.py
+++ data_gen_toymodel.py
@@ -10,45 +10,15 @@ np.set_printoptions(precision=3, linewidth=150)
 The toy model is captured at the level of 2D distribution of E_nu, the neutrion energy and Q^2, momentum 
 """
 
-c = 1.0
-
-E_mu_peak = 0.6  # [GeV]
-E_mu_width = 0.1  # [GeV]
-
-M_p = 0.938  # [GeV]
-M_n = 0.939  # [GeV]
-
-M_l = 0.106  # [GeV]  Mass of a muon
-
-
-# def Detection_efficiency(mom, threshold = 1.5e-4):
-#     """
-#     Args:
-#         threshold: detection threshold
-#
-#     Returns:
-#
-#     """
-#     def response_func(x, Amp,  mid, width):
-#         return Amp/2 * (np.tanh((x - mid)/width)+1)
-#
-#     mid_detecor = 1.5e-4
-#     width_detecor = 1e-6
-#
-#     def Stochaistic_acceptance(trh=threshold ):
-#
-#         p_detect = response_func(mom, Amp=1,
-#                                  mid=mid_detecor,
-#                                  width=width_detecor)
-#
-#         click_array = rng.choice(a=[0.0, 1.0], p=[1-p_detect, p_detect])
-#
-#
 
 def safe_sqrt(x):
     with np.errstate(invalid='ignore'):  # Ignore invalid value warnings
         return np.sqrt(np.maximum(x, 0))
 
+def safe_divide(numerator, denominator):
+    with np.errstate(divide='ignore', invalid='ignore'):
+        result = np.divide(numerator, denominator)
+    return result
 
 def get_3Mom_magn(M, E):
     return safe_sqrt(E ** 2 - M ** 2 * c ** 4) / c
@@ -82,22 +52,36 @@ def ToyModel(mean_E_nu, std_E_nu, kappa, N_events):
     Q = rng.exponential(scale=Q_dropoff)
 
     # Using the kinematics constraints the proton and lepton energies and 3-momenta magnitudes are determined
-    E_p = (-Q ** 2 + c ** 2 * (M_n ** 2 + M_p ** 2))  # proton energy form kinematic constraints
-    E_l = (- Q ** 2 + c ** 2 * (M_n ** 2 + M_p ** 2)) / (2 * M_n)  # charged lepton energy form energy conservation
+    E_p = (-Q ** 2 + c ** 2 * (M_n ** 2 + M_p ** 2)) / (2*M_n) # proton energy form kinematic constraints
+    E_p = Q ** 2 / (2 * M_n) + M_n * c ** 2
+    E_l = E_nu + M_n * c ** 2 - E_p   # charged lepton energy form energy conservation
 
     # obtaining the 3 momenta magnitudes from the energies
     p_p = get_3Mom_magn(M_p, E_p)
     p_l = get_3Mom_magn(M_l, E_l)
 
+    # Angular reconstruction
+    # right now the code is sufficiently fast for its intended purpose reserving the angular reconstruction calculation
+    # is possible but not worth the effort at this point
+    def lepton_angle(E_l, E_nu, p_l):
+        numerator = M_p ** 2 - M_n ** 2 - M_l ** 2 + 2 * E_l * M_n - 2 * M_n * E_nu + 2 * E_l * E_nu
+        denominator = 2 * p_l * E_nu
+        cos_theta = safe_divide(numerator, denominator)
+        return cos_theta
+
+    def proton_angle(E_p, E_nu, p_p):
+        numerator = M_l ** 2 - M_n ** 2 - M_p ** 2 + 2 * E_p * M_n - 2 * M_n * E_nu + 2 * E_p * E_nu
+        denominator = 2 * p_p * E_nu
+        cos_theta = safe_divide(numerator, denominator)
+        return cos_theta
+
+    cos_theta_p = proton_angle(E_p, E_nu, p_p)
+    cos_theta_l = lepton_angle(E_l, E_nu, p_p)
+
     # constructing the data array shape = (6, 10)
 
-    data_arr = np.column_stack((E_nu, Q, E_p, p_p, E_l, p_l))
-
+    data_arr = np.column_stack((E_nu, Q, E_p, p_p, E_l, p_l, cos_theta_p, cos_theta_l))
     return data_arr
-
-
-d_arr = ToyModel(E_mu_peak, E_mu_width, 3, 10)
-
 
 
 def Efficiency_func(p, eff_max, p_th, nu, xi, delt, phi):
@@ -138,9 +122,6 @@ def DetectorAcceptance(data_array):
     p_l = data_array[:, 5]
 
 
-
-
-
     p_th = 2.5  # [GeV] i. e. 250 MeV
     eff_max = 1.0
     nu = 1.0
@@ -163,9 +144,96 @@ def DetectorAcceptance(data_array):
     detected_data[:, 4] = np.where(lepton_reject_status, np.nan, detected_data[:, 4])
     detected_data[:, 5] = np.where(lepton_reject_status, np.nan, detected_data[:, 5])
 
+    return detected_data
+
+
+if __name__ == '__main__':
+
+    """Physical parameters"""
+    c = 1.0
+    E_mu_peak = 0.6  # [GeV]
+    E_mu_width = 0.1  # [GeV]
+    kappa = 3.0
+    N_events = 5000
+
+    M_p = 0.938  # [GeV]
+    M_n = 0.939  # [GeV]
+    M_l = 0.106  # [GeV]  Mass of a muon
+
+    """Plotting parameters"""
+    all_data_c = "#21A124"
+    full_reconst_c = "#D0231F"
+    proton_reconst_c = "#FF7E00"
+    lepton_reconst_c = "#1975B6"
+
+    axis_font_size = 13
+
+    data = ToyModel(E_mu_peak, E_mu_width, kappa, N_events)
+
+    detected_data = DetectorAcceptance(data)
+
+    fully_reconstructed_condition = np.logical_not(np.isnan(detected_data).any(axis=1))
+    fully_reconstructed_data = detected_data[fully_reconstructed_condition]
+
+    ########################################################################
+
+    fig1 = plt.figure(figsize=(12,8))
+    ax11 = fig1.add_subplot(121)
+    ax12 = fig1.add_subplot(122)
+
+    ax11.hist(data[:, 0], color=all_data_c)
+    ax11.hist(fully_reconstructed_data[:, 0], color=full_reconst_c)
+    ax11.grid()
+    ax11.set_xlabel(r"$E_{\nu}$", fontsize=axis_font_size)
+    ax11.set_ylabel("count", fontsize=axis_font_size)
+
+    ax12.hist(data[:, 1] ** 2, color=all_data_c)
+    ax12.hist(fully_reconstructed_data[:, 1] ** 2, color=full_reconst_c)
+    ax12.grid()
+    ax12.set_xlabel(r"$Q^2$", fontsize=axis_font_size)
+    ax12.set_ylabel("count", fontsize=axis_font_size)
+
+    ########################################################################
+
+    fig2 = plt.figure(figsize=(12, 8))
+    ax21 = fig2.add_subplot(121)
+    ax22 = fig2.add_subplot(122)
+
+    ax21.hist(data[:, 2], color=all_data_c)
+    ax21.hist(fully_reconstructed_data[:, 2], color=full_reconst_c)
+    ax21.grid()
+    ax21.set_xlabel(r"$E_{p}$", fontsize=axis_font_size)
+    ax21.set_ylabel("count", fontsize=axis_font_size)
+
+    ax22.hist(data[:, 3], color=all_data_c)
+    ax22.hist(fully_reconstructed_data[:, 3], color=full_reconst_c)
+    ax22.grid()
+    ax22.set_xlabel(r"$|\vec{p}_p|$", fontsize=axis_font_size)
+    ax22.set_ylabel("count", fontsize=axis_font_size)
+
+    ########################################################################
+
+    fig3 = plt.figure(figsize=(10, 4))
+    ax31 = fig3.add_subplot(121)
+    ax32 = fig3.add_subplot(122)
+
+    ax31.hist(data[:, 4], color=all_data_c)
+    ax31.hist(fully_reconstructed_data[:, 4], color=full_reconst_c)
+    ax31.grid()
+    ax31.set_xlabel(r"$E_{l}$", fontsize=axis_font_size)
+    ax31.set_ylabel("count", fontsize=axis_font_size)
+
+    ax32.hist(data[:, 5], color=all_data_c)
+    ax32.hist(fully_reconstructed_data[:, 5], color=full_reconst_c)
+    ax32.grid()
+    ax32.set_xlabel(r"$|\vec{p}_l|$", fontsize=axis_font_size)
+    ax32.set_ylabel("count", fontsize=axis_font_size)
+
+    ########################################################################
+
+    plt.show()
 
 
 
 
 
-DetectorAcceptance(d_arr)
